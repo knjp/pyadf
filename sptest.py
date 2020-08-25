3debe93b786bdff407b1f98f026b726474b4f43b
kiyoshi Nishikawa <kiyoshi.nishikawa@gmail.com>
2020-08-25 20:00:23 +0900

    Added a new method plot

--- sptest.py
+++ sptest.py
@@ -2,13 +2,15 @@ import spmisc
 import numpy as np
 
 x = spmisc.spmisc()
-d = np.ones(20)
-x.plotFreq(d)
+d = np.ones(200)
+#x.plotFreq(d)
 
-num = 500
+num = 100
 r1 = np.random.randn(num)
-x = np.linspace(0,num-1, num)
+xaxis = np.linspace(0,num-1, num)
 r2 = np.random.randn(num)
-print(r1)
+#print(r1)
 r3 = np.roll(r1,1)
-print(r3)
+#print(r3)
+
+x.plot(r2)
