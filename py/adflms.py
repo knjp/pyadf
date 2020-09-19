88d873ebd7defc966fe40293c1c0487f1feba500
Kiyoshi Nishikawa <kiyoshi.nishikawa@gmail.com>
2020-09-19 17:40:37 +0900

    The setter for the parameter mu is added

--- py/adflms.py
+++ py/adflms.py
@@ -7,6 +7,17 @@ class lmsalgorithm(adfalgorithm.adfalgorithm):
         self._mu = 0.01
         self._name = "LMS algorithm"
 
+    @property
+    def mu(self):
+        return self._mu
+    
+    @mu.setter
+    def mu(self, value):
+        if value > 0:
+            self._mu = value
+        else:
+            print('The step size must be positive!')
+            self._mu = 0.01
 
     def update(self, e):
         self._wcoef = self._wcoef + self._mu * e * self._winput
